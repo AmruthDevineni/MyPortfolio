8a03ecfd2d699e25bb7dca7569add593e69a0640
AmrDevBU <amrdev@bu.edu>
2025-06-04 11:26:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MyPortfolio/',  // ← important: must match your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
